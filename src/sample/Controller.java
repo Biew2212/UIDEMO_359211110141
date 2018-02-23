61bf6b1282683616965df90a5ff2c4416b9ae400
Biew2212 <biew_dd36@hotmail.com>
2018-02-23 11:08:55 +0700

    login

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,41 @@
 package sample;
 
-public class Controller {
-}
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXTextField;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Controller implements Initializable {
+    private loginModel loginModel = new loginModel();
+
+    @FXML
+    private JFXTextField username;
+
+    @FXML
+    private JFXTextField passwoeg;
+
+    @FXML
+    private JFXButton btnLogin;
+
+    @FXML
+    private Label loginStatus;
+
+    @FXML
+    private Label dbstatus;
+
+
+
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+    if (this.loginModel.isDatabaseConnection()){
+        this.dbstatus.setText("Connected to DB.");
+    }else {
+        this.dbstatus.setText("Not Connect to DB.");
+    }
+    }
+}//class
